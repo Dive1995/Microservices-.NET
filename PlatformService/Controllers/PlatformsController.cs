387576f9624fa152f086d440f104f6c71298c07d
Diveshan <diveshan1995@gmail.com>
2022-09-21 07:13:04 +0530

    initial commit

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controller
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms(){
+            var platforms = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            Console.WriteLine("id : " + id);
+            var platform = _repository.GetPlatformById(id);
+            Console.WriteLine(_mapper.Map<PlatformReadDto>(platform));
+            if(platform != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platform));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var createdPlatformDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = createdPlatformDto.Id}, createdPlatformDto);
+        }
+    }
+}
